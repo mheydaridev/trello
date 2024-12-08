0fd599fc404b9a49cf64b1680fdec0619849c13f
Mahdi Heydari <mheydaridev@gmail.com>
2024-12-08 12:34:26 +0330

    add bootstrap v5.3 to project

--- src/views/project/index.php
+++ src/views/project/index.php
@@ -8,12 +8,14 @@ use yii\helpers\Html;
         <li role="presentation" class="dropdown" id="menu_archive_list">
             <?= $this->render('_menu_archive_list',['archive_statuses'=>$archive_statuses]); ?>
         </li>
-
         <li role="notification" class="dropdown" id="menu_notification_list">
             <?= $this->render('_menu_notification_list',['archive_statuses'=>$archive_statuses]); ?>
         </li>
         <li>
             <a href="#" class="icon-list" onclick="return showPopOverTeam(this);" data-pop-id="#pop-menu-menu" title="<?= Module::t("module","Show Menu") ?>"><i class="fa fa-align-justify" data-pop-id="#pop-menu-menu"></i> </a>
+        </li>
+        <li>
+            <a href="#" class="icon-list" onclick="return showPopOverTeam(this);" data-pop-id="#pop-menu-team" title="<?= Module::t("module","Create New Team") ?>"><i class="fa fa-plus" data-pop-id="#pop-menu-team"></i> </a>
         </li>
         <li>
             <a href="#" class="icon-list" onclick="return showPopOverTeam(this);" data-pop-id="#pop-menu-team" title="<?= Module::t("module","Create New Team") ?>"><i class="fa fa-plus" data-pop-id="#pop-menu-team"></i> </a>
